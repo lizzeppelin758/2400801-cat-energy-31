4a7bd75f30aad8a87c9b49a41651f33c60fad11b
lizzeppelin758 <lizzeppelin758@gmail.com>
2024-05-03 11:49:56 +0600

    Добавляет состояния интерактивных элементов и скрипт меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const buttonMenu = document.querySelector('.main-header__navigation-toggle');
+const menu = document.querySelector('.main-header__navigation');
+
+buttonMenu.addEventListener('click', () =>{
+  const display = menu.style.display;
+  if(display === 'none' || display === '') {
+    menu.style.display = 'flex';
+    buttonMenu.classList.add('main-header__navigation-toggle--close');
+  } else {
+    menu.style.display = 'none';
+    buttonMenu.classList.remove('main-header__navigation-toggle--close');
+  }
+});
+
