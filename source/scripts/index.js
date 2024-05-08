0bab37659e471b5117692c87899bc342c10bd283
lizzeppelin758 <lizzeppelin758@gmail.com>
2024-05-08 11:19:53 +0600

    Добавляет адаптивные сетки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,13 +2,20 @@
 const buttonMenu = document.querySelector('.main-header__navigation-toggle');
 const menu = document.querySelector('.main-header__navigation');
 
-buttonMenu.addEventListener('click', () =>{
-  const display = menu.style.display;
-  if(display === 'none' || display === '') {
-    menu.style.display = 'flex';
-    buttonMenu.classList.add('main-header__navigation-toggle--close');
-  } else {
-    menu.style.display = 'none';
-    buttonMenu.classList.remove('main-header__navigation-toggle--close');
-  }
-});
+const mqLarge = window.matchMedia('(max-width: 767px)');
+if (mqLarge.matches){
+  buttonMenu.classList.add('main-header__navigation-toggle--open');
+  menu.classList.add('main-header__navigation--close');
+
+  buttonMenu.addEventListener('click', () =>{
+    if(menu.classList.contains('main-header__navigation--close')) {
+      menu.classList.remove('main-header__navigation--close');
+      menu.classList.add('main-header__navigation--open');
+      buttonMenu.classList.add('main-header__navigation-toggle--close');
+    } else {
+      menu.classList.remove('main-header__navigation--open');
+      menu.classList.add('main-header__navigation--close');
+      buttonMenu.classList.remove('main-header__navigation-toggle--close');
+    }
+  });
+}
